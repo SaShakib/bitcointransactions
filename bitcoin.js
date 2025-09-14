7186983ea6ffce53340f795d839c33ed13f84727
Shakib <sashakibuhbc@gmail.com>
2025-09-14 04:41:01 +0000

    top 10 transactions

--- bitcoin.js
+++ bitcoin.js
@@ -1,9 +1,27 @@
 const https = require("https");
+const fs = require("fs");
+
+/**
+ * detailed breakthough,,, using rpc call method getbestblockhash i am getting first hash of the blockchain.
+ * then from each hash i am getting transaction using getblock using params blockhash and 2 for transaction informations. 
+ * then filtering out the coinbase we are taking real transactions. and adding them together. each block
+ * contains some transactions we are adding them. getting the hash 
+ * and update the blockhash to move backward 
+ * but when time is less cutoff we break the cycle
+ * 
+ * 
+ * so basically we are starting from latest time and ending it in 1 hour timeframe. 
+ * 
+ * sort the transactions and take first 10. 
+ * 
+ */
+
+
 
 const RPC_HOST = "bold-aged-spring.btc.quiknode.pro";
 const RPC_PATH = "/d7976324e8b8030d07e84a05db8fecb73bca6ce5/";
 
-// Simple RPC wrapper
+
 function rpcCall(method, params = []) {
   const data = JSON.stringify({
     method,
@@ -45,13 +63,15 @@ async function getLastHourTopTxs() {
   const cutoff = Math.floor(Date.now() / 1000) - 3600; // 1 hour ago
   let blockHash = await rpcCall("getbestblockhash");
   let txs = [];
+  console.log(blockHash);
+
 
   while (blockHash) {
-    const block = await rpcCall("getblock", [blockHash, 2]); // verbosity=2 for tx details
+    const block = await rpcCall("getblock", [blockHash, 2]);
     if (block.time < cutoff) break;
 
     for (const tx of block.tx) {
-      // skip coinbase (first tx in block)
+      
       if (tx.vin.some((vin) => vin.coinbase)) continue;
 
       const totalOut = tx.vout.reduce((sum, v) => sum + (v.value || 0), 0);
@@ -61,15 +81,17 @@ async function getLastHourTopTxs() {
         time: block.time,
         value: totalOut,
       });
+
+      
     }
 
     blockHash = block.previousblockhash;
   }
 
-  // sort by value, descending
   txs.sort((a, b) => b.value - a.value);
 
   return txs.slice(0, 10);
+  
 }
 
 // Run
@@ -77,5 +99,9 @@ getLastHourTopTxs()
   .then((topTxs) => {
     console.log("Top 10 transactions in last hour:");
     console.table(topTxs);
+
+    const header = "txid,blockhash,time,value\n";
+    const rows = topTxs.map(t => `${t.txid},${t.blockhash},${t.time},${t.value}`).join("\n");
+    fs.writeFileSync("top10_transactions.csv", header + rows, "utf8");
   })
   .catch(console.error);
